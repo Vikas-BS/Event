49591e0d21d9548c89a0e32d6cf828a57f89d274
Vikas-BS <vikas.sundaresh@gmail.com>
2025-06-27 15:24:03 +0530

    commit

--- event-calendar/vite.config.js
+++ event-calendar/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/event-calendar/',
+  base: '/Event/',
   plugins: [react()],
   resolve: {
     alias: {

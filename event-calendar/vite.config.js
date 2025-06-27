48acb94577c7ba364a2c82f045e46e0e2e26a22b
Vikas-BS <vikas.sundaresh@gmail.com>
2025-06-27 16:22:27 +0530

    config

--- event-calendar/vite.config.js
+++ event-calendar/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
+  base: '/', // ✅ required for Vercel
   plugins: [react()],
-  base: process.env.VITE_BASE-PATH || "/Event"
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
